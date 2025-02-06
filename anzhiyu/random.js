db305c1506fcb047b092c64fc59eb7a3f60b0b21
starfalldown <18516066675@163.com>
2025-02-06 21:23:28 +0800

    Site updated: 2025-02-06 21:23:28

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2025/02/03/N1～N5-Certification Standards-Japanese Language Proficiency Test-JLPT/","2022/02/01/lmi all MIUI13 development version internal test flash packages before the Spring Festival/","2022/01/31/Some random talks about 2022/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
