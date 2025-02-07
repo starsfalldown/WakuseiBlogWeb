a15d71f755f4182ec5f66dec8d610df9cc0aea8c
starfalldown <18516066675@163.com>
2025-02-07 14:55:43 +0800

    Site updated: 2025-02-07 14:55:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/03/N1～N5-Certification Standards-Japanese Language Proficiency Test-JLPT/","2022/01/31/Some random talks about 2022/","2022/02/01/lmi all MIUI13 development version internal test flash packages before the Spring Festival/","2025/02/06/The most comprehensive report on Trump's tariff plan in the Simplified Chinese Internet might be here/"];function toRandomPost(){
+var posts=["2025/02/03/N1～N5-Certification Standards-Japanese Language Proficiency Test-JLPT/","2022/02/01/lmi all MIUI13 development version internal test flash packages before the Spring Festival/","2022/01/31/Some random talks about 2022/","2025/02/06/The most comprehensive report on Trump's tariff plan in the Simplified Chinese Internet might be here/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
